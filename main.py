1ff72b2ecb2065ef33e3bc373fdfc61902fa54f6
Windpath <windpath@hotmail.ca>
2020-05-16 11:37:16 -0400

    Updated index and imports

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import flask
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, make_response, redirect, url_for
 
 app = Flask(__name__)
 
@@ -7,19 +7,19 @@ app = Flask(__name__)
 
 @app.route('/start')
 def start():
-    return flask.render_template('input_form.html')
+    return render_template('input_form.html')
 
 
 """ Sets client information to the request object's cookies and prompts emergency form
 Access cookies: flask.request.cookies (type:dict) """
 
 # NOTE: Still need to make emergency form default the contact number to 911 if the person chooses not to respond
-@app.route('/set_user_info', methods=['POST', 'GET'])
+@app.route('/set_user_info/', methods=['POST', 'GET'])
 def set_user_info():
-    if flask.request.method == 'POST':
-        name, age, drug_type = flask.request.form.values()
+    if request.method == 'POST':
+        name, age, drug_type = request.form.values()
 
-        resp = flask.make_response(flask.render_template('emergency_form.html', name=name))
+        resp = flask.make_response(render_template('emergency_form.html', name=name))
         resp.set_cookie('Name', name)
         resp.set_cookie('Age', age)
         resp.set_cookie('Drug', drug_type)
@@ -30,12 +30,12 @@ def set_user_info():
 """ Sets the emergency contact information to the request object's cookies and prompts timer interface
 Access cookies: flask.request.cookies (type:dict) """
 
-@app.route('/timer', methods=['POST', 'GET'])
+@app.route('/timer/', methods=['POST', 'GET'])
 def timer():
-    if flask.request.method == 'POST':
-        c_name, c_phone = flask.request.form.values()
+    if request.method == 'POST':
+        c_name, c_phone = request.form.values()
 
-        c_resp = flask.make_response(flask.render_template('timer.html'))
+        c_resp = make_response(render_template('timer.html'))
         c_resp.set_cookie('Contact Name', c_name)
         c_resp.set_cookie('Contact Phone', c_phone)
 
@@ -44,9 +44,20 @@ def timer():
 
 """ Home page to redirect to other pages (ie. input form or information)"""
 @app.route('/')
-def home():
+def index():
     #TODO
-    return flask.render_template('home_page.html')
+    # if request.method == 'POST':
+    #     if request.form['submit_button'] == "Record Info":
+    #         return redirect(url_for('start'))
+    #     elif request.form['submit_button'] == "Safety Timer":
+    #         return redirect(url_for('timer'))
+    #     elif request.form['submit_button'] == "Nearby Resources":
+    #         return redirect(url_for("map"))
+    #     elif request.form['submit_button'] == "Information":
+    #         return redirect(url_for("info"))
+    # elif request.method == 'GET':
+    #     return render_template('index.html')
+    return render_template('index.html')
 
 """ Helpful locations map to link to safe injection facilities,
 needle exchanges, Supervised Consumption Services, take-home naxolone centers,
